c377155022439a53599bf0bc5b4cd4a8d471ee4f
PowerNether <64768544+PowerNether@users.noreply.github.com>
2020-12-18 18:35:58 +0500

    fix

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
-$(document).ready(function(){
-  [].forEach.call(
-    document.querySelectorAll(".item__image"),
-    function (el) {
-      tns({
-        container: el,
-        items: 1,
-        nav: true,
-        controls: false,
-        fixedWidth: 360,
-        mouseDrag: true,
-      });
-    }
-  );
-  $(".sorting__view").on("click tap", function() {
-    $(".sorting__view").removeClass("active")
-    $(this).addClass("active")
-  })
-  $(".sorting__quantites").on("click tap", function() {
-    $(".sorting__quantites").removeClass("active")
-    $(this).addClass("active")
-  })
-  $('.tabs').tabs();
-  $('select').formSelect();
+[].forEach.call(document.querySelectorAll(".item__image"), function (el) {
+  tns({
+    container: el,
+    items: 1,
+    nav: true,
+    controls: false,
+    fixedWidth: 360,
+    mouseDrag: true,
+  });
 });
+$(".sorting__view").on("click tap", function () {
+  $(".sorting__view").removeClass("active");
+  $(this).addClass("active");
+});
+$(".sorting__quantites").on("click tap", function () {
+  $(".sorting__quantites").removeClass("active");
+  $(this).addClass("active");
+});
+$("select").formSelect();
+$(".tabs").tabs();
